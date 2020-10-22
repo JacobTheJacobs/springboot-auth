ec5639c2262d8f26750ee810e95fcf1ba0aa86ae
unknown <jake.yanov@gmail.com>
2020-10-22 04:04:33 +0300

    second commit

--- src/main/java/com/example/demo/controllers/TestController.java
+++ src/main/java/com/example/demo/controllers/TestController.java
@@ -1,15 +1,26 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.models.User;
+import com.example.demo.repository.UserRepository;
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
+	
+	
+	@Autowired
+	UserRepository userRepository;
+
 	@GetMapping("/all")
 	public String allAccess() {
 		return "Public Content.";
@@ -28,8 +39,8 @@ public class TestController {
 	}
 
 	@GetMapping("/admin")
-	@PreAuthorize("hasRole('ADMIN')")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public String adminAccess() {
-		return "Admin Board.";
+		return "Adminm Board.";
 	}
 }
---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,71 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.User;
+import com.example.demo.payload.SignupRequest;
+import com.example.demo.repository.UserRepository;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/test")
+public class UserController {
+	
+	@Autowired
+	UserRepository userRepository;
+
+
+	@GetMapping("/users")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public List<User> getUsers() {
+		List<User> findAll = userRepository.findAll();
+		return findAll;
+	}
+	
+	@PostMapping("/users")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public void addEmployee(@RequestBody User user) {
+		System.out.println(user);
+		userRepository.save(user);
+	}
+	
+	@GetMapping("/users/{id}")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public User getSingleUser(@PathVariable Long id) {
+		return userRepository.findById(id).get();
+	}
+	
+	@PutMapping("/users/{id}")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public void updateUser(@RequestBody User user, @PathVariable Long id) {
+		User findById = userRepository.findById(id).get();
+		System.out.println(findById);
+		findById.setUsername(user.getUsername());
+		findById.setEmail(user.getEmail());
+		findById.setPassword(user.getPassword());
+		userRepository.save(findById);
+
+	}
+	
+	@DeleteMapping("/users/{id}")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public void deleteUser(@PathVariable Long id) {
+		userRepository.deleteById(id);
+	}
+	
+
+}
